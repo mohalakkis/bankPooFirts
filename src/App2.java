43de11baa18a140c13fbf418550a72b20cbdfd6f
mohamed.lakkis <mohamed.lakkis@consultores.rnp.br>
2025-02-22 19:58:11 -0300

    feat: teste novos obj.

---
+++ src/App2.java
@@ -0,0 +1,11 @@
+public class App2 {
+public static  void main() {
+    Account a = null;
+    if (a != null ) {
+        a.printBalance();
+    }else {
+        System.out.println("não da");
+    }
+
+}
+}
